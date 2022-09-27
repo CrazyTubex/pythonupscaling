2aa7b7a4b1dd90e3e2d1852948583bdd0426e6c1
tubex <lukagamermaker@gmail.com>
2022-09-27 22:20:49 +0200

    added test for image_write

--- src/tests/upscale_test.py
+++ src/tests/upscale_test.py
@@ -1,10 +1,11 @@
 from scripts.setup import setup
 from scripts.upscale import upscale
 from scripts.upscale import read_image
+from scripts.upscale import write_image
 import unittest
 from pathlib import Path
 import os
-
+import cv2
 
 
 
@@ -14,8 +15,12 @@ class TestUpScale(unittest.TestCase):
         self.assertNotEqual(setup()[1], False)
     def test_read_image(self):
         self.assertEqual(read_image('imageforunittest.png', (str(Path(os.getcwd()).parent)+'/input/'))[1], True)
+    def test_image_write(self):
+        self.assertNotEqual(write_image('imageforunittest.png', cv2.imread((str(Path(os.getcwd()).parent)+'/input/'+'imageforunittest.png')),(str(Path(os.getcwd()).parent)+'/output/')), False)
     def test_upscale(self):
         self.assertEqual(upscale(setup()[0], 'imageforunittest.png', (str(Path(os.getcwd()).parent)+'/input/'), (str(Path(os.getcwd()).parent)+'/output/')), True)
+    
+
         
     
 
