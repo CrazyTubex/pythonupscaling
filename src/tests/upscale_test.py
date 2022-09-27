3209b1ba0f02e2de47ae1829d69f33a0dcba86c2
tubex <lukagamermaker@gmail.com>
2022-09-27 21:05:20 +0200

    test ver 1.1

--- src/tests/upscale_test.py
+++ src/tests/upscale_test.py
@@ -1,5 +1,6 @@
 from scripts.setup import setup
 from scripts.upscale import upscale
+from scripts.upscale import read_image
 import unittest
 
 
@@ -8,6 +9,8 @@ import unittest
 class TestUpScale(unittest.TestCase):
     def test_setup(self):
         self.assertNotEqual(setup(), False)
+    def test_read_image(self):
+        self.assertEqual(read_image('imageforunittest.png')[1], True)
     def test_upscale(self):
         self.assertEqual(upscale(setup(), 'imageforunittest.png'), True)
         
